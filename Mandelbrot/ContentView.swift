8d52a6ae2b625ed4b1434c168d4dd371c131634e
sinn246 <sinn246@hotmail.com>
2020-06-04 12:32:59 +0900

    Initial Commit

---
+++ Mandelbrot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mandelbrot
+//
+//  Created by 西村信一 on 2020/06/04.
+//  Copyright © 2020 sinn246. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
